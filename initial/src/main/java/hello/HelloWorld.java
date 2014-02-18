393988b5e62f63a87c1763a76cf8f7384949fbcf
CPUT Student <stud@ubuntu.(none)>
2014-02-18 08:07:35 -0800

    Added a note

--- initial/src/main/java/hello/HelloWorld.java
+++ initial/src/main/java/hello/HelloWorld.java
@@ -1,5 +1,10 @@
 package hello;
-
+/*
+	This is my first attempt to commit on GIT
+	File: HelloWorld.java
+	Date: 18 February 2014
+	Author: Bradley Joseph(210016299) Today
+*/
 public class HelloWorld {
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
